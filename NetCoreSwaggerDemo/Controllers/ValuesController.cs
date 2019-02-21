39b22f7adf32f3822d9a63cbd44441c5f6774b87
WuChaoZhi <wuchaozhi@yun-kai.com>
2019-02-21 22:01:32 +0800

    init

---
+++ NetCoreSwaggerDemo/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreSwaggerDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        /// <summary>
+        /// Request Values by Id
+        /// </summary>
+        /// <remarks>
+        /// Example:
+        /// Get api/values/123
+        /// </remarks>
+        /// <param name="id">request id</param>
+        /// <returns>values match id</returns>
+        /// <response code="201">empty value</response>
+        /// <response code="400">id is null</response>
+        [HttpGet("{id}")]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult<string> Get(int id)
+        {
+            return "Request Id" + id;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NetCoreSwaggerDemo/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace NetCoreSwaggerDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c=>{
+                c.SwaggerDoc("v1",new Info{
+                    Title="Aikenwu API",Version="V1"
+                    ,Description="A simple api made by Aikenwu"
+                    ,TermsOfService="None"
+                    ,Contact=new Contact(){
+                        Name="Aikenwu",
+                        Email="Aikenwu@qq.com",
+                        Url="https://github.com/Aikenwu"
+                    },
+                    License=new License{
+                        Name="MIT License",
+                        Url="https://github.com/Aikenwu"
+                    }
+                    });
+                var basePath = Path.GetDirectoryName(AppContext.BaseDirectory);
+                var xmlPath = Path.Combine(basePath, "NetCoreSwaggerDemo.xml");
+                c.IncludeXmlComments(xmlPath);
+
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //app.UseHsts();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c=>{
+                c.SwaggerEndpoint("/swagger/v1/swagger.json","Aikenwu API V1");
+                //c.RoutePrefix=string.Empty;
+            });
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
